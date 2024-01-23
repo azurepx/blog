b8e1b9fe46239147ecf82d67016167883ebf3d5f
azurepx <azurepan213@gmail.com>
2024-01-23 16:53:08 +0800

    config for deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -2,21 +2,28 @@ import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
+
+const site = {
+  name: 'Azure PX',
+  url: 'azure-px.github.io',
+  description: `Azure PX is a collection of Azure's`
+}
+
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: site.name,
+  tagline: 'MtF, she/her',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: `https://${site.url}`,
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'azurepx', // Usually your GitHub org/user name.
+  projectName: site.name, // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
